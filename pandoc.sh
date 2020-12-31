ef3742dbbbaa5adea010feab535c3b105a04b305
jerry-sky <jwroczynski@gmail.com>
2020-12-31 16:35:32 +0100

    pandoc script: removed unnecessary comments that was containing outdated information
    Generally, that is why you should keep detailed explanation in one place — e.g. the readme file.

--- pandoc.sh
+++ pandoc.sh
@@ -5,8 +5,6 @@ input_file="$1"
 # path to the pandoc template file
 template="$2"
 # the name of the styles files (CSS file)
-## please note — this is NOT a full path to the file,
-## the full path will be resolved with respect to the `<base>` tag defined in the `<head>` of the HTML output document
 stylesheet_filename="$3"
 # path to the headers file (additional headers to be included inside the `<head>` element in the output HTML document)
 headers="$4"
