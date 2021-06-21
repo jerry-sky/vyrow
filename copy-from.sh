90b1e5ada16e78c36f80e73dfb65b2409e660695
jerry-sky <jwroczynski@gmail.com>
2021-06-21 18:09:51 +0200

    fixed the `copy-from` script and the `rename-index` script

--- copy-from.sh
+++ copy-from.sh
@@ -7,11 +7,23 @@ if [ -n "$src_dir" ]; then
     # given source directory is not empty
 
     # get the absolute paths of the source and working directories
-    wrk_dir=$(readlink -f "$wrk_dir")
-    src_dir=$(readlink -f "$src_dir")
+    wrk_dir=$(readlink -f -- "$wrk_dir")
+    src_dir=$(readlink -f -- "$src_dir")
 
-    # get the list of all files to copy excluding those
-    # that are the working directory itself or the `.git` directory
-    find "$src_dir" -type f | grep -v ^"$wrk_dir" | grep -v '\/\.git\/' \
-        | xargs -d '\n' cp -r -t "$wrk_dir" --
+    if [ "$wrk_dir" = "$src_dir" ]; then
+        echo "::warning::working directory is the same as the source directory"
+        exit 0
+    fi
+
+    exclude=""
+
+    echo "$wrk_dir" | grep ^"$src_dir" >/dev/null
+    if [ "$?" -eq 0 ]; then
+        # the source directory *does* contain the working directory
+        # exclude the working directory from copying
+        exclude="${wrk_dir#$src_dir}"
+    fi
+
+    rsync -r "$src_dir"/{*,.[!.]?*} "$wrk_dir" --exclude .git --exclude "$exclude"
+
 fi
